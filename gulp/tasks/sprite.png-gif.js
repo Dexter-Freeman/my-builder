65f245fb096bc32dc17aa7f9c659b11b8a3989c6
Dexter-Freeman <vse.pisma.tut@gmail.com>
2017-04-15 22:13:37 +0300

    Второй коммит

--- gulp/tasks/sprite.png-gif.js
+++ gulp/tasks/sprite.png-gif.js
@@ -1,15 +1,35 @@
 'use strict';
 
+
+
 module.exports = function() {
   $.gulp.task('sprite:png-gif', function() {
-    return $.gulp.src('./source/sprite/*.png}')
-      .pipe($.gp.spritesmith({
-        imgName: 'sprite.png',
-        cssName: 'sprite-png.scss',
-        padding: 2
-      }))
-     $.gt.gutil.log('sprite take files')
-      .pipe($.gulp.dest($.config.root + '/assets/img'))
-      .on('error', $.gp.notify.onError({ title: 'sprite:png-gif' }))
-  })
+
+    let merge = require('merge-stream');
+
+    let spriteData = $.gulp.src('./source/sprite/*.png')
+    .pipe($.gp.spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'sprite-png.scss',
+      padding: 2
+    }));
+
+
+    let cssStream = spriteData.css
+    .pipe($.gulp.dest('./source/style'));
+
+    let imgStream = spriteData.img
+   // .pipe($.gp.imagemin())
+   .pipe($.gulp.dest($.config.root + '/assets/img'));
+
+
+   return merge(imgStream, cssStream);
+
+// return
+//     spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img')); // Путь куда сохраняем картинку
+//     spriteData.css.pipe($.gulp.dest('./source/style')); // Путь куда сохраняем стили
+
+
+
+})
 };
